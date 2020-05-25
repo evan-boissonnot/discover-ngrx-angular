4380f4bdcba9edd62a69ab9e12e6555cb76d4de8
Evan Boissonnot <evan@boissonnot.fr>
2020-05-25 16:11:25 +0200

    evo: two components with same state

--- test-counter/src/app/test/test.component.ts
+++ test-counter/src/app/test/test.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import * as fromStore from '../store/reducers';
+import { Observable } from 'rxjs';
+import { counterSelector } from '../store/selectors/counter.selectors';
 
 @Component({
   selector: 'app-test',
@@ -8,8 +10,11 @@ import * as fromStore from '../store/reducers';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+  count$: Observable<number>;
 
-  constructor(private store: Store<fromStore.State>) { }
+  constructor(private store: Store<fromStore.State>) {
+    this.count$ = store.pipe(select(counterSelector));
+  }
 
   ngOnInit(): void {
   }
