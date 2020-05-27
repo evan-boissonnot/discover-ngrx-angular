31d3699590f1ba8aee0620b9b0e25660626d955e
Evan Boissonnot <evan@boissonnot.fr>
2020-05-27 18:48:12 +0200

    evo: second way to dispatc with action

--- test-one/src/app/wookie-add/wookie-add.component.ts
+++ test-one/src/app/wookie-add/wookie-add.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromStore from '../store/reducers';
-import { WookiesActionsType } from '../store/actions/wookie.actions';
+import { WookiesActionsType, addWookie } from '../store/actions/wookie.actions';
 import { Wookie } from '../core/models/wookie';
 
 @Component({
@@ -18,7 +18,8 @@ export class WookieAddComponent implements OnInit {
   }
 
   addOne(): void {
-    this.store.dispatch( { type: WookiesActionsType.addOne, wookie: this.wookie } );
+    // two ways to dispatch
+    this.store.dispatch( addWookie({ wookie: this.wookie} )); // { type: WookiesActionsType.addOne, wookie: this.wookie } );
     this.wookie = new Wookie();
   }
 }
